fc8a44a5a8b44994dfc81152507f0405b0a37465
typicalmike002 <townsendm105@gmail.com>
2015-11-14 20:48:31 -0500

    Added Sass

--- themes/digitalportfolio/Gruntfile.js
+++ themes/digitalportfolio/Gruntfile.js
@@ -7,7 +7,53 @@ module.exports = function(grunt) {
 
 		pkg: grunt.file.readJSON('package.json'),
 
+		watch: {
+			options: {
+				livereload: true,
+			},
+			scss: {
+				files: ['css/sass/*.scss', 'css/sass/**/*.scss'],
+				tasks: ['compass', 'combine_mq', 'cssmin'],
+				options: {
+					spawn: false
+				}
+			}
+		},
 
+		compass: {
+			dist: {
+				options: {
+					config: 'config.rb',
+					sassDir: 'css/sass',
+					cssDir: 'css',
+					environment: 'development'
+				}
+			}
+		},
 
+		combine_mq: {
+			options: {
+				beautify: false
+			},
+			main: {
+				src: 'style.css',
+				dest: 'style.css'
+			}
+		},
+
+		cssmin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'css/sass',
+					src: ['*.css']
+				}]
+			}
+		}
 	});
+	grunt.registerTask('default', [
+		'grunt-contrib-cssmin',
+		'grunt-contrib-combine-mq',
+		'grunt-contrib-compass'
+	]);
 };
