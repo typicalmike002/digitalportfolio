0c1d942662c12aa793d1db9e37e3e6bc8499955c
typicalmike002 <townsendm105@gmail.com>
2015-11-15 15:46:43 -0500

    media category filter

--- themes/digitalportfolio/functions.php
+++ themes/digitalportfolio/functions.php
@@ -57,11 +57,11 @@ function inject_scripts() {
 	$js_dir = get_template_directory_uri() . '/js';
 
 	//Loads requirejs, we set true to push everything into the footer.
-	wp_enqueue_script( 'requrejs', $js_dir . '/require.js', '', true );
+	wp_enqueue_script( 'requrejs', $js_dir . '/require.js', '', '', true );
 
 
 	//requirejs config file that depends on requirejs.
-	wp_register_script( 'configjs', $js_dir . '/config.js', 'requirejs', true );
+	wp_register_script( 'configjs', $js_dir . '/config.js', 'requirejs', '', true );
 
 
 	//Creates a json object so the configjs knows its directory. 
@@ -70,50 +70,10 @@ function inject_scripts() {
 	));
 
 
-	wp_enqueue_script( 'configjs','', '', true );
+	wp_enqueue_script( 'configjs', '', '', '', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'inject_scripts' );
-
-
-
-
-/**
- * Configures the WordPress dashboard by adding/removing specific menu pages.
- *
- * @uses add_menu_page() For adding new menu pages to the dashboard.
- * @uses remove_menu_page() For removing default WordPress dashboard menus.
- *
- * @since Digital Portfolio 0.1
- */
-function dashboard_menu() {
-
-	//Removes the post type and comments from the WordPress dashboard.
-	remove_menu_page( 'edit.php' );
-	remove_menu_page( 'edit-comments.php' );
-
-}
-add_action( 'admin_menu', 'dashboard_menu' );
-
-
-
-
-/**
- * Configures the WordPress Admin bar.
- *
- * @uses remove_node() to remove unneeded nodes from the admin bar.
- *
- * @since Digital Portfolio 0.1
- */
-function admin_bar_options() {
-
-	global $wp_admin_bar;
-
-	//Removes  Add New post.
-	$wp_admin_bar->remove_node( 'new-post' );
-
-}
-add_action( 'wp_before_admin_bar_render', 'admin_bar_options' );
 
 
 
@@ -129,6 +89,7 @@ function add_taxonomies() {
 
 	//Adds categories for attachments.
 	register_taxonomy_for_object_type( 'category', 'attachment' );
+	apply_filters( 'wpmediacategory_taxonomy' , 'category' );
 
 	//Adds categories for pages.
 	register_taxonomy_for_object_type( 'category', 'page');
@@ -141,7 +102,7 @@ add_action( 'init', 'add_taxonomies' );
 
 
 /**
- * Removes wp_emojicons from slowing our theme down.
+ * Removes wp_emojicons from loading in the <head>.
  *
  * @since digital Portfolio 0.1
  */
@@ -163,4 +124,141 @@ function disable_emojicons() {
 }
 add_action( 'init', 'disable_emojicons');
 
-?>
+
+
+
+
+//Code only to be loaded for the admin section.
+if ( is_admin() ) { 
+
+
+
+
+	/**
+	 * Custom walker for wp_dropdown_categories.  
+	 * Based on https://github.com/wp-plugins/wp-media-library-categories/blob/master/index.php
+	 * This code allows the admin to filter photos by category in the media library.
+	 *
+	 * @since Digital Portfolio 0.2
+	 */
+	class media_category_filter extends Walker_CategoryDropdown {
+
+		function option_list( &$output, $category, $depth = 0, $args = array(), $id = 0 ) {
+			
+			$pad = str_repeat( '&nbsp;', $depth * 3 );
+			$cat_name = apply_filters( 'list_cats', $category->name, $category );
+
+			if( ! isset( $args['value'] ) ) {
+				$args['value'] = ( $category->taxonomy != 'category' ? 'slug' : 'id' );
+			}
+
+			$value = ( $args['value'] == 'slug' ? $category->slug : $category->term_id );
+			if ( 0 == $args['selected'] && isset( $_GET['category_media'] ) && '' != $_GET['category_media'] ) {
+				$args['selected'] = $_GET['category_media'];
+			}
+
+			$output .= '<option class="level-' . $depth . '" value="' . $value . '"';
+			if ( $value === (string) $args['selected'] ) {
+				$output .= ' selected="selected"';
+			}
+
+			$output .= '>';
+			$output .= $pad . $cat_name;
+			if ( $args['show_count'] )
+				$output .= '&nbsp;&nbsp;( ' . $category->count . ')';
+
+			$output .= "</option>\n";
+		}
+	}
+
+
+
+
+	/**
+	 * Usees the media_category_filter class to create the category filter.
+	 * Based on https://github.com/wp-plugins/wp-media-library-categories/blob/master/index.php
+	 *
+	 * @since  Digital Portfolio 0.2
+	 */
+	function add_media_category_filter() {
+		
+		global $pagenow;
+		
+		if ( 'upload.php' == $pagenow ) {
+			
+			// Default taxonomy
+			$taxonomy = 'category';
+
+			// Add filter to change the default taxonomy.
+			$taxonomy = apply_filters( 'wpmediacategory_taxonomy', $taxonomy );
+			
+			if ( $taxonomy != 'category' ) {
+				$dropdown_options = array(
+					'taxonomy'			=>	$taxonomy,
+					'name'				=>	$taxonomy,
+					'show_option_all'	=>	__( 'View all categories' ),
+					'hide_empty'		=>	false,
+					'hierarchical'		=>	true,
+					'orderby'			=>	'name',
+					'show_count'		=>	true,
+					'walker'			=>	new media_category_filter(),
+					'value'				=>	'slug'
+				);
+			} else {
+				$dropdown_options = array(
+					'taxonomy'			=>	$taxonomy,
+					'show_option_all'	=>	__( 'View all categories' ),
+					'hide_empty'		=>	false,
+					'hierarchical'		=>	true,
+					'orderby'			=>	'name',
+					'show_count'		=>	false,
+					'walker'			=>	new media_category_filter(),
+					'value'				=>	'id'
+				);
+			}
+			wp_dropdown_categories( $dropdown_options );
+		}
+	}
+	add_action( 'restrict_manage_posts', 'add_media_category_filter' );
+	
+
+
+
+	/**
+	 * Configures the WordPress dashboard by adding/removing specific menu pages.
+	 *
+	 * @uses add_menu_page() For adding new menu pages to the dashboard.
+	 * @uses remove_menu_page() For removing default WordPress dashboard menus.
+	 *
+	 * @since Digital Portfolio 0.1
+	 */
+	function dashboard_menu() {
+
+		//Removes the post type and comments from the WordPress dashboard.
+		remove_menu_page( 'edit.php' );
+		remove_menu_page( 'edit-comments.php' );
+
+	}
+	add_action( 'admin_menu', 'dashboard_menu' );
+
+
+
+
+	/**
+	 * Configures the WordPress Admin bar.
+	 *
+	 * @uses remove_node() to remove unneeded nodes from the admin bar.
+	 *
+	 * @since Digital Portfolio 0.1
+	 */
+	function admin_bar_options() {
+
+		global $wp_admin_bar;
+
+		//Removes  Add New post.
+		$wp_admin_bar->remove_node( 'new-post' );
+
+	}
+	add_action( 'wp_before_admin_bar_render', 'admin_bar_options' );
+	
+} ?>
