1d9d9ee22aa1537b676130a7bc4a831ea165bc95
typicalmike002 <townsendm105@gmail.com>
2015-11-21 11:38:23 -0500

    controls loop through images when max/min index is reached

--- themes/digitalportfolio/gallery-controller.php
+++ themes/digitalportfolio/gallery-controller.php
@@ -6,51 +6,52 @@
  *
  * @package  WordPress
  * @subpackage  Digital Portfolio
- * @version 0.3
+ * @version 0.31
 */
 
-//Start the gallery with a session with a single variable that controls 
-//the image that should be displayed.
+//This gallery uses a php session that saves the index number of the image being
+//currently viewed.  This php code acts as a fallback for people without javascript.
 session_start();
 
-//Retrieve all galleries of this gallery post type.
-$gallery = get_post_gallery_images( $post );
-
-//Store the highest image array index.
-$gallery_length = count($gallery);
-$gallery_length--;
-
+//Session variable to remember what image our user is viewing.  Defaults to 0.
 if (empty($_SESSION['image_index']) ) {
 	
-	$_SESSION['image_index'] = 0;
+	 $_SESSION['image_index'] = 0;
 }
 
 
-if (isset($_POST['gallery_controller'])) {
+$index = $_SESSION['image_index'];
 
-	if ($_POST['gallery_controller'] == 'left' && $_SESSION['image_index'] > 0 ) {
+
+//Retrieve all images from the gallery.
+$gallery = get_post_gallery_images( $post );
+
+
+//Highest index number in gallery array.
+$gallery_length = count( $gallery );
+$gallery_length--;
+
+
+//When form class gallery submits, change the image_index up or down. 
+if ( isset( $_POST['control_action'] ) ) {
+
+	if ( $_POST['control_action'] === 'left' ) {
 	
-		$_SESSION['image_index']--;
-	
+		$_SESSION['image_index'] = $index > 0 ? $index -= 1 : $gallery_length;
+		
 	}
 
-	if ($_POST['gallery_controller'] == 'right' && $_SESSION['image_index'] < $gallery_length ) {
+	if ( $_POST['control_action'] === 'right' ) {
 
-		$_SESSION['image_index']++;
+		$_SESSION['image_index'] = $index < $gallery_length ? $index += 1 : 0;
 
 	}
-}
 
- ?>
+} ?>
 
-<div class="gallery">
-	<form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
-			<div class="gallery_control--left">
-				<button name="gallery_controller" type="submit" value="left">Left</button>
-			</div>
-			<?php echo '<img src="' . $gallery[$_SESSION['image_index']] . '" class="gallery_image">'; ?>
-			<div class="gallery_control--right">
-				<button name="gallery_controller" type="submit" value="right">Right</button>
-			</div>
-	</form>
-</div>
+<form class="gallery" action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+	<img class="gallery_image" src="<?php echo $gallery[$_SESSION['image_index']]; ?>" ><br/>
+	<button class="gallery_button" name="control_action" type="submit" value="left" >Left</button>
+	<button class="gallery_button" name="control_action" type="submit" value="right" >Right</button>
+</form>
+
