23b8552dca90fdce5495ae4049617d9294482631
typicalmike002 <townsendm105@gmail.com>
2015-12-20 18:02:11 -0500

    Repo updated on: Sun Dec 20 2015 18:02:05

---
+++ themes/digitalportfolio/js/modules/event_listeners.js
@@ -0,0 +1,16 @@
+define(function(){
+
+	'use strict';
+
+	var listeners = {
+		'prevent_default': function(event){
+			event.preventDefault();
+		},
+		'get_url': function(element){
+			var url = element.getAttribute('href')''
+			return url;
+		},
+		''
+	};	
+
+});
