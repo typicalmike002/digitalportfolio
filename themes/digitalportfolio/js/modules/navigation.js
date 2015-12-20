9f885f1cff8d20b3fe9f47d6da128eee8a8c5a80
typicalmike002 <townsendm105@gmail.com>
2015-12-20 17:41:53 -0500

    Repo updated on: Sun Dec 20 2015 17:41:47

--- themes/digitalportfolio/js/modules/navigation.js
+++ themes/digitalportfolio/js/modules/navigation.js
@@ -6,15 +6,10 @@ define(function(require) {
 
 	nav.get_links = function(){
 		var div = document.getElementById('nav'),
-			links = div.getElementsByTagName('a'),
-			anchor = [];
-
-		for (var i = 0, l = links.length; i < l; i++){
-			anchor = links[i];
-		}
+			links = div.getElementsByTagName('a');
 
 		return links;
-
+		
 	};
 
 	return nav;
