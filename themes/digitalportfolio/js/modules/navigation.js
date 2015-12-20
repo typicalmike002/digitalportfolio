7aeb2c22360e1ee49729b0105d87cea3b694391f
typicalmike002 <townsendm105@gmail.com>
2015-12-20 17:44:49 -0500

    Repo updated on: Sun Dec 20 2015 17:44:43

--- themes/digitalportfolio/js/modules/navigation.js
+++ themes/digitalportfolio/js/modules/navigation.js
@@ -7,9 +7,12 @@ define(function(require) {
 	nav.get_links = function(id){
 		var div = document.getElementById('nav'),
 			links = div.getElementsByTagName('a');
-
-		return links[id];
-
+		
+		if ( links.length < id ){
+			return 'Array out of bounds error';
+		} else {
+			return links[id];
+		}
 	};
 
 	return nav;
