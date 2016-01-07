c5253712d75b187744f54c5df25e887a93beb8c9
Michael Townsend <typicalmike002@Michaels-MacBook-Air.local>
2016-01-07 17:58:09 -0500

    Repo updated on: Thu Jan 07 2016 17:58:04

--- themes/digitalportfolio/js/modules/app.js
+++ themes/digitalportfolio/js/modules/app.js
@@ -9,10 +9,14 @@ define(function (require) {
 	//Enables ajax request for navigation links:
 	for (var i = 0, l = nav.links_length(); i < l; i++ ) {
 		var ajax_enabled_nav = nav.get_links(i);
-		ajax_enabled_nav.addEventListener('click', function(event){
-			event.preventDefault();
-			ajax.load_content( nav.get_href(this) );
-			ajax.push_state( nav.get_href(this) );
-		}, false);
+		if (typeof window.addEventListener === 'function'){
+			(function () {
+				ajax_enabled_nav.addEventListener('click', function(event){
+					event.preventDefault();
+					ajax.load_content( nav.get_href(this) );
+					ajax.push_state( nav.get_href(this) );
+				}, false);
+			}());
+		}
 	}
 });
